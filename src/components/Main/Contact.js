8da2c44cf004b1367e1d4022cd1ca30da701f11d
saaeg657 <ssaeg657@gmail.com>
2018-01-20 21:14:51 +0900

    add email

--- src/components/Main/Contact.js
+++ src/components/Main/Contact.js
@@ -15,6 +15,7 @@ class Contact extends React.Component {
   render() {
     return (
       <div style={styles.contents}>
+        <h1>ssaeg657@gmail.com</h1>
         {/* <img src='https://cdn.mirror.wiki/http://www.dsb.kr/data/chinesehansi/1228643372-22.jpg' style={styles.img} />
         <h1>Developer : Kim Jong-deok, the Kuyam<a href="http://cafe.daum.net/kuyam"> (http://cafe.daum.net/kuyam) </a></h1> */}
       </div>
